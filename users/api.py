b8ecc30856ad493ef65a42d62bcb0cd9ca633efb
Johnny <butter3126@gmail.com>
2022-03-01 02:45:24 +0800

    feat: modify auth and create user api

--- users/api.py
+++ users/api.py
@@ -5,39 +5,77 @@ from ninja import Router
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
+from django.db import transaction
 
 from services.auth_service import AuthJWT
-from errors import AuthFail
+from services.oauth_service import OauthStrategy
 
-from .models import User
-from .schemas import AuthIn, AuthOut, UserIn, UserOut
+from errors import AuthFail, APIException
+
+from .models import User, OauthRecord
+from .schemas import AuthIn, AuthOut, UserIn, UserOut, RegisterUserIn
 
 
 router = Router()
 
 @router.post("/auth", response=AuthOut)
 def auth(request, payload: AuthIn):
-    user = authenticate(request, username=payload.username, password=payload.password)
+    user = None
+
+    if payload.oauth_type:
+        try:
+            oauthed_user = OauthStrategy(oauth_type=payload.oauth_type).auth(payload.oauth_code)
+        except ValueError as e:
+            raise APIException(message=str(e))
+
+        oauth_record = get_object_or_404(OauthRecord, oauth_type=payload.oauth_type, oauth_id=oauthed_user.user_id)
+        user = oauth_record.user
+    else:
+        user = authenticate(request, username=payload.username, password=payload.password)
 
     if user is None:
-        raise AuthFail
+        raise AuthFail()
 
-    payload = {'user_id': user.id}
-
-    jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    jwt_token = _get_jwt_token(user)
 
     return {'token': jwt_token}
 
 
-@router.post('', response=UserOut)
-def create_user(request, payload: UserIn):
-    password = payload.dict().pop('password')
+@router.post('', response=AuthOut)
+def create_user(request, payload: RegisterUserIn):
+    user = None
 
-    user = User.objects.create(**payload.dict())
-    user.set_password(password)
-    user.save()
+    if (payload.oauth_code):
+        try:
+            oauthed_user = OauthStrategy(oauth_type=payload.oauth_type).auth(payload.oauth_code)
+        except ValueError as e:
+            raise APIException(message=str(e))
 
-    return user
+        with transaction.atomic():
+            oauth_record = OauthRecord.objects.create(
+                oauth_type = payload.oauth_type,
+                oauth_id = oauthed_user.user_id,
+                user = User.objects.create(
+                    username = User.get_random_username(),
+                    password = '',
+                    email = oauthed_user.email,
+                ),
+            )
+
+            user = oauth_record.user
+    else:
+        password = payload.dict().pop('password')
+
+        user = User.objects.create(**payload.dict(exclude={'oauth_type', 'oauth_code'}))
+        user.set_password(password)
+        user.save()
+
+    if user is None:
+        raise APIException(message='register fail, please check!')
+
+    jwt_token = _get_jwt_token(user)
+
+    return {'token': jwt_token}
 
 
 @router.get('', response=UserOut, auth=AuthJWT())
@@ -63,3 +101,11 @@ def update_user(request, payload: UserIn):
     user.save()
 
     return user
+
+
+def _get_jwt_token(user: User):
+    payload = {'user_id': user.id}
+
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+    return token
--- users/models.py
+++ users/models.py
@@ -1,9 +1,17 @@
+import random
+import string
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     email = models.CharField(unique=True, null=True, max_length=254)
+
+
+    @staticmethod
+    def get_random_username() -> str:
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
 
 
 class OauthRecord(models.Model):
