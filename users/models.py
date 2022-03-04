eaa10f984ecdbb353cff2ae04574cd8d221395cf
Johnny <butter3126@gmail.com>
2022-03-05 03:46:59 +0800

    style: flake8 rule

--- users/models.py
+++ users/models.py
@@ -8,7 +8,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.CharField(unique=True, null=True, max_length=254)
 
-
     @staticmethod
     def get_random_username() -> str:
         return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
@@ -18,7 +17,6 @@ class OauthRecord(models.Model):
     oauth_type = models.CharField(max_length=16)
     oauth_id = models.CharField(unique=True, null=True, max_length=24)
     user = models.ForeignKey('User', models.CASCADE)
-
 
     class Meta:
         unique_together = [['oauth_type', 'oauth_id']]
